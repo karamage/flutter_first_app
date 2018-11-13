59a90bc0db18979e286c380888a20c1dde1e7095
Masaaki Kakimoto <karamage.umauma@gmail.com>
2018-11-13 20:31:43 +0900

    friendly chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,149 @@
 // Create an infinite scrolling lazily loaded list
 
+//TODO 11/20  7. Animate your appの最初からやる
+
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new FriendlychatApp());
+  /*
+  runApp(
+    new MaterialApp(
+      title: "Friendlychat",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Friendlychat"),
+        ),
+      ),
+    ),
+  );
+  */
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendlychat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController(); //new
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(
+              color: Theme.of(context).cardColor
+            ),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
+    /*
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat"),),
+      body: _buildTextComposer(),
+    );
+    */
+  }
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                  hintText: "Send a message"
+                ),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message)
+    });
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  final String _name = "karamage";
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          // 6. Add a UI for displaying messages
+          //TODO new Column  //ここから続き
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              )
+            ],
+          )
+        ],
+      )
+    );
+  }
+}
+
+
+/*
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
@@ -110,3 +253,4 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 }
+*/
